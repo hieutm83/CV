c5712ec1fb1326dced040bd767da3857042f6230
hieutm83 <tolahatdoxanh@gmail.com>
2024-12-16 15:16:16 +0700

    Update ProfileProjects.tsx

--- src/app/ProfileProjects.tsx
+++ src/app/ProfileProjects.tsx
@@ -45,7 +45,7 @@ export default function ProfileProjects() {
                 TikTok Shop
               </h2>
             </div>
-            <div className="space-y-8">
+            <div className="space-y-6">
               <ProjectItem
                 name="Lắp Đặt Camera Hà Nội"
                 url="https://www.tiktok.com/@ldchanoi"
@@ -81,7 +81,7 @@ export default function ProfileProjects() {
                 Shopee
               </h2>
             </div>
-            <div className="space-y-8">
+            <div className="space-y-6">
               <ProjectItem
                 name="Lắp Đặt Camera Hà Nội"
                 url="https://shopee.vn/lapdatcamerahanoi_ldc"
@@ -117,23 +117,17 @@ export default function ProfileProjects() {
                 Lazada
               </h2>
             </div>
-            <div className="space-y-8"> {/* Tăng khoảng cách giữa các mục */}
+            <div className="space-y-6"> {/* Tăng khoảng cách giữa các mục */}
               <ProjectItem
                 name="Lắp Đặt Camera Hà Nội"
-                url="https://www.lazada.vn/lapdatcamerahanoi_ldc"
+                url="https://www.lazada.vn/shop/lap-dat-camera-ha-noi/?spm=a2o4n.pdp_revamp.seller.1.49932882T5pm13&itemId=2787171787&channelSource=pdp"
                 imageUrl="https://p16-sign-sg.tiktokcdn.com/aweme/1080x1080/tos-alisg-avt-0068/9973881cfae458c32e5bb2f35a381741.jpeg?lk3s=a5d48078&nonce=97328&refresh_token=a949dfaac730d318103341800013e1e3&x-expires=1734490800&x-signature=hGZdFbWDusUyOMT%2B6QF83s7yp9U%3D&shp=a5d48078&shcp=81f88b70"
                 borderColor="border-[#00aff0]"
               />
               <ProjectItem
                 name="Camera Gia Đình"
-                url="https://www.lazada.vn/camera86giadinh"
+                url="https://www.lazada.vn/shop/x710lojf/?spm=a2o4n.pdp_revamp.seller.1.217c37e2U1PqyD&itemId=2569842199&channelSource=pdp"
                 imageUrl="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f1d81a6964884377c92697e475f4402f~c5_1080x1080.jpeg?lk3s=a5d48078&nonce=3604&refresh_token=0e565e481c66a32e25ce72269a255e8e&x-expires=1734490800&x-signature=yEJeLiLrbSLgc4aFStQ02ee5lfM%3D&shp=a5d48078&shcp=81f88b70"
-                borderColor="border-[#00aff0]"
-              />
-              <ProjectItem
-                name="KHOCONGNGHE.86"
-                url="https://www.lazada.vn/khocongnghe86"
-                imageUrl="https://p16-sign-sg.tiktokcdn.com/aweme/1080x1080/tos-alisg-avt-0068/bd4160940411c1904a636023f5cbedff.jpeg?lk3s=a5d48078&nonce=99124&refresh_token=f6c62c4bdbafa8749858781d4b85ca21&x-expires=1734490800&x-signature=QNifYN8BJAIiS7eTFsLyb%2F34JT0%3D&shp=a5d48078&shcp=81f88b70"
                 borderColor="border-[#00aff0]"
               />
             </div>
